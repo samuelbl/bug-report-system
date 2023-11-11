40558a059045e429be69064f404d7041f5248289
samuelbl <samuel.bristot-loli@eurodyn.com>
2023-11-11 16:09:52 -0300

    implement bug-list

--- src/app/components/bug-list/bug-list.component.ts
+++ src/app/components/bug-list/bug-list.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Bug } from 'src/app/domain/bug';
+import { BugService } from 'src/app/service/bug.service';
 
 @Component({
   selector: 'app-bug-list',
   templateUrl: './bug-list.component.html',
   styleUrls: ['./bug-list.component.css']
 })
-export class BugListComponent {
+export class BugListComponent implements OnInit{
+  bugs: Bug[] = [];
+  sortingField: string = 'title';
+  isAscending: boolean = true;
+
+  constructor(private bugService: BugService){}
+
+  ngOnInit(): void {
+    this.loadBugs();
+  }
+
+  loadBugs(){
+    this.bugService.getBugs(this.sortingField, this.isAscending ? 'asc' : 'desc').subscribe(data => {
+      this.bugs = data;
+    });
+  }
+
+  sort(field: string){
+    console.log("field = " + field);
+    console.log("sorting field = " + this.sortingField);
+    if(this.sortingField === field){
+      console.log("isAscending = " + this.isAscending);
+      this.isAscending = !this.isAscending
+      console.log("isAscending after changed = " + this.isAscending);
+    }else{
+      this.sortingField = field;
+      this.isAscending = true;
+    }
+    this.loadBugs();
+  }
 
 }
--- src/app/service/bug.service.ts
+++ src/app/service/bug.service.ts
@@ -1,9 +1,26 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Bug } from '../domain/bug';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BugService {
 
-  constructor() { }
+  baseUrl = "http://localhost:3000/bugs";
+
+  constructor(private http: HttpClient) { }
+
+  getBugs(sortField: string, sortOrder: string){
+    const params = new HttpParams()
+    .set('_sort', sortField)
+    .set('_order', sortOrder);
+    
+    return this.http.get<Bug[]>(this.baseUrl, {params});
+  }
+
+  createBug(bugData: Bug) {
+    return this.http.post<Bug>(this.baseUrl, bugData);
+  }
+
 }
